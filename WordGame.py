1a60dca868312542d8afd6cfd81cf63de75811b2
Yoseph <c00158579@itcarlow.ie>
2015-03-20 09:32:33 +0000

    1
    1

--- WordGame.py
+++ WordGame.py
@@ -98,4 +98,6 @@ def ranking():
                            home_link = url_for("again"), )
 
 app.config['SECRET_KEY'] = 'thisismysecretkeywhichyouwillneverguesshahahahahahahaha'
-app.run(debug=True)
+if __name__ == "__main__":
+ 	port = int(os.environ.get("PORT", 5000))
+    	app.run(host='0.0.0.0', port=port)
